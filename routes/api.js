858325d5d8e300d7d611e527889c42a29822afd5
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-28 02:36:15 +0200

    basic functionality
    - simple inputextraction/ checks
    - use proxy for stock prices
    - add empty tests

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,31 @@
 'use strict';
 
+const proxy_url = sym => `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${sym}/quote`
+
+async function get_stocks(stocks, res) {
+  const results = [];
+  for (const stock of stocks) {
+    results.push(await fetch(proxy_url(stock)).then(y => y.json()))
+  }
+  res.json(results)
+}
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(function (req, res) {
+      let stock = req.query.stock;
+      const like = req.query.like;
+      if (!stock) {
+        res.status(400).json('stock required')
+      }
+      if (typeof stock == "string") {
+        stock = [stock]
+      }
+      if (!stock.every(x => /^[a-zA-Z]{2,8}$/.test(x))) {
+        res.status(400).json('nope')
+      }
+      get_stocks(stock, res)
     });
-    
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,9 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    test('Viewing one stock: GET request to /api/stock-prices/')
+    test('Viewing one stock and liking it: GET request to /api/stock-prices/')
+    test('Viewing the same stock and liking it again: GET request to /api/stock-prices/')
+    test('Viewing two stocks: GET request to /api/stock-prices/')
+    test('Viewing two stocks and liking them: GET request to /api/stock-prices/')
 });
