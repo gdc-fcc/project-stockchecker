d3368fb19552b15ff58e41b61c3d3caeb3215297
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-07-01 06:46:49 +0200

    implement functional tests (no like count)

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,10 +5,73 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-    test('Viewing one stock: GET request to /api/stock-prices/')
-    test('Viewing one stock and liking it: GET request to /api/stock-prices/')
-    test('Viewing the same stock and liking it again: GET request to /api/stock-prices/')
-    test('Viewing two stocks: GET request to /api/stock-prices/')
-    test('Viewing two stocks and liking them: GET request to /api/stock-prices/')
+suite('Functional Tests', () => {
+    test('Viewing one stock: GET request to /api/stock-prices/', done => {
+        chai
+            .request(server)
+            .get('/api/stock-prices?stock=goog&like=false')
+            .end((_err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "stockData")
+                assert.equal(res.body.stockData.stock, "goog")
+                assert.property(res.body.stockData, "likes")
+                done()
+            })
+    })
+    test('Viewing one stock and liking it: GET request to /api/stock-prices/', done => {
+        chai
+            .request(server)
+            .get('/api/stock-prices?stock=goog&like=true')
+            .end((_err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "stockData")
+                assert.equal(res.body.stockData.stock, "goog")
+                assert.property(res.body.stockData, "likes")
+                done()
+            })
+    })
+    test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', done => {
+        chai
+            .request(server)
+            .get('/api/stock-prices?stock=goog&like=true')
+            .send({ stock: 'GOOG', like: true })
+            .end((_err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "stockData")
+                assert.equal(res.body.stockData.stock, "goog")
+                assert.property(res.body.stockData, "likes")
+                done()
+            })
+    })
+    test('Viewing two stocks: GET request to /api/stock-prices/', done => {
+        chai
+            .request(server)
+            .get('/api/stock-prices?stock=goog&stock=msfit&like=true')
+            .send({ stock: ['goog', 'msfit'], like: true })
+            .end((_err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "stockData")
+                assert.typeOf(res.body.stockData, "array")
+                assert.equal(res.body.stockData.length, 2)
+                assert.equal(res.body.stockData[0].stock, "goog")
+                assert.property(res.body.stockData[1], "rel_likes")
+                done()
+
+            })
+    })
+    test('Viewing two stocks and liking them: GET request to /api/stock-prices/', done => {
+        chai
+            .request(server)
+            .get('/api/stock-prices?stock=goog&stock=msfit&like=true')
+            .send({ stock: ['goog', 'msfit'], like: true })
+            .end((_err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "stockData")
+                assert.typeOf(res.body.stockData, "array")
+                assert.equal(res.body.stockData.length, 2)
+                assert.equal(res.body.stockData[0].stock, "goog")
+                assert.property(res.body.stockData[1], "rel_likes")
+                done()
+            })
+    })
 });
